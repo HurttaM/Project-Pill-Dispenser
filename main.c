833cdac27b875d367e0fb411b9fdd3da44af09aa
Iida <iidamaija.pirttiniemi@gmail.com>
2025-11-21 17:38:57 +0200

    Definitions moved to header file

--- main.c
+++ main.c
@@ -1,59 +1,4 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "iuart.h"
-
-#define BUTT0 9
-#define BUTT1 8
-#define BUTT2 7
-#define UART_NR 1
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-#define BAUD_RATE 9600
-#define TIMEOUT_WAIT 500
-#define MOTOR_REV_DET 28
-#define MOTOR_1 2
-#define MOTOR_2 3
-#define MOTOR_3 6
-#define MOTOR_4 13
-#define MOTOR_STEPS 8
-
-const int pins[4] = {MOTOR_1, MOTOR_2, MOTOR_3, MOTOR_4};
-const int step1[4] = {1, 0, 0, 0};
-const int step2[4] = {1, 1, 0, 0};
-const int step3[4] = {0, 1, 0, 0};
-const int step4[4] = {0, 1, 1, 0};
-const int step5[4] = {0, 0, 1, 0};
-const int step6[4] = {0, 0, 1, 1};
-const int step7[4] = {0, 0, 0, 1};
-const int step8[4] = {1, 0, 0, 1};
-const int *sequences[MOTOR_STEPS] = {step1, step2, step3, step4, step5, step6, step7, step8};
-
-void setup() {
-    stdio_init_all();
-    gpio_init(MOTOR_1);
-    gpio_init(MOTOR_2);
-    gpio_init(MOTOR_3);
-    gpio_init(MOTOR_4);
-    gpio_init(BUTT0);
-    gpio_init(BUTT1);
-    gpio_init(BUTT2);
-    gpio_init(MOTOR_REV_DET);
-    gpio_set_dir(MOTOR_1, true);
-    gpio_set_dir(MOTOR_2, true);
-    gpio_set_dir(MOTOR_3, true);
-    gpio_set_dir(MOTOR_4, true);
-    gpio_set_dir(MOTOR_REV_DET, GPIO_IN);
-    gpio_set_dir(BUTT0, GPIO_IN);
-    gpio_set_dir(BUTT1, GPIO_IN);
-    gpio_set_dir(BUTT2, GPIO_IN);
-    gpio_pull_up(BUTT0);
-    gpio_pull_up(BUTT1);
-    gpio_pull_up(BUTT2);
-    gpio_pull_up(MOTOR_REV_DET);
-    iuart_setup(UART_NR, UART_TX_PIN, UART_RX_PIN, BAUD_RATE);
-    //gpio_set_irq_enabled_with_callback(MOTOR_REV_DET, GPIO_IRQ_EDGE_FALL, true, falling);
-}
-
+#include "pill-dispenser.h"
 
 /*
 Projektin (vähimmäis)tavoitteet:
@@ -72,24 +17,35 @@ Logiikka:
 
 int main() {
 
-    const uint led_pin = 22;
-    uint count = 0;
+    // initializing everything here:
 
     // Initialize LED pin
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
+    gpio_init(LED1);
+    gpio_set_dir(LED1, GPIO_OUT);
 
     // Initialize chosen serial port
     stdio_init_all();
 
+
+
+    uint count = 0; // for testing (printing)
+
+
     // Loop forever
     while (true) {
 
-        // Blink LED
+        // waits for a button to be pressed and blinks a led
         printf("Blinking! %u\r\n", ++count);
-        // gpio_put(led_pin, true);
+        gpio_put(LED1, true);
         sleep_ms(1000);
-        gpio_put(led_pin, false);
+        gpio_put(LED1, false);
         sleep_ms(1000);
+
+        // if button pressed, calibrates itself
+
+
+
+
+
     }
 }
--- pill-dispenser.c
+++ pill-dispenser.c
@@ -1,3 +1,82 @@
-//
-// Created by Iida on 21/11/2025.
-//
+#include "pill-dispenser.h"
+
+int steps[8][4] = { // 8 steps, 4 motors -> multidim array
+    {1, 0, 0, 0},
+    {1, 1, 0, 0},
+    {0, 1, 0, 0},
+    {0, 1, 1, 0},
+    {0, 0, 1, 0},
+    {0, 0, 1, 1},
+    {0, 0, 0, 1},
+    {1, 0, 0, 1}
+};
+
+int calibrate(void) {
+    int results[3];
+    int step_index = 0;
+    printf(">> Calibrating... <<\n");
+    int motor_steps = 0;
+
+    for (int edge = 0; edge < 4; edge++) {
+        motor_steps = 0;
+        int prev = gpio_get(OPTOFORK);
+        int curr = prev;
+
+        while (!(prev == 1 && curr == 0)) {
+            prev = curr;
+
+            step_motor(step_index);
+            step_index = (step_index + 1) % 8;
+            sleep_ms(1.5); // 1 tulee liikaa, 2 tulee täsmälleen 4096, 1.5 tulee 4100 esim
+
+            curr = gpio_get(OPTOFORK);
+            motor_steps++;
+        }
+        if (edge > 0) {
+            printf("%d. revolution: %d steps\n", edge, motor_steps);
+        }
+        if (edge > 0) {
+            results[edge - 1] = motor_steps;
+        }
+    }
+    int average = (results[0] + results[1] + results[2]) / 3;
+    printf("Average step count per revolution = %d\n", average);
+    return motor_steps;
+}
+
+void set_up_gpio(void) {
+    gpio_init(OPTOFORK);
+    gpio_set_dir(OPTOFORK, GPIO_IN);
+    gpio_pull_up(OPTOFORK);
+
+    gpio_init(STEPM1);
+    gpio_set_dir(STEPM1, GPIO_OUT);
+    gpio_init(STEPM2);
+    gpio_set_dir(STEPM2, GPIO_OUT);
+    gpio_init(STEPM3);
+    gpio_set_dir(STEPM3, GPIO_OUT);
+    gpio_init(STEPM4);
+    gpio_set_dir(STEPM4, GPIO_OUT);
+}
+
+void step_motor(int index) {
+    gpio_put(STEPM1, steps[index][0]);
+    gpio_put(STEPM2, steps[index][1]);
+    gpio_put(STEPM3, steps[index][2]);
+    gpio_put(STEPM4, steps[index][3]);
+}
+
+int run_motor(int run_times, int motor_steps) {
+    int take_steps_amount = run_times * motor_steps / 8;
+    int counter = 0;
+
+    int step_index = 0;
+    for (int i = 0; i < take_steps_amount; i++) {
+        step_motor(step_index);
+        step_index = (step_index + 1) % 8;
+        counter++;
+        sleep_ms(2);
+    }
+    return counter;
+}
+
