8b9587130bf6260579f08208eec273a8fc899ce6
Iida <iidamaija.pirttiniemi@gmail.com>
2025-11-21 18:53:10 +0200

    Functions added: pressed(), calibrate(), set_up_gpio(), step_motor(), run_motor()

--- pill-dispenser.c
+++ pill-dispenser.c
@@ -11,36 +11,53 @@ int steps[8][4] = { // 8 steps, 4 motors -> multidim array
     {1, 0, 0, 1}
 };
 
+bool pressed(int button) {
+    int press = 0;
+    int release = 0;
+    while(press < 3 && release < 3) {
+        if(gpio_get(button) == 0) {
+            press++;
+            release = 0;
+        } else {
+            release++;
+            press = 0;
+        }
+        sleep_ms(10); // wait 10 ms
+    }
+    if (press > release) {
+        return true;
+    }
+    return false;
+}
+
 int calibrate(void) {
-    int results[3];
-    int step_index = 0;
     printf(">> Calibrating... <<\n");
     int motor_steps = 0;
 
-    for (int edge = 0; edge < 4; edge++) {
+    for (int i = 0; i < 2; i++) {
         motor_steps = 0;
-        int prev = gpio_get(OPTOFORK);
-        int curr = prev;
+        int last = gpio_get(OPTOFORK);
+        int curr = last;
 
-        while (!(prev == 1 && curr == 0)) {
-            prev = curr;
+        while (!(last == 1 && curr == 0)) {
+            last = curr;
 
-            step_motor(step_index);
-            step_index = (step_index + 1) % 8;
-            sleep_ms(1.5); // 1 tulee liikaa, 2 tulee täsmälleen 4096, 1.5 tulee 4100 esim
+            step_motor();
+            sleep_ms(1);
 
             curr = gpio_get(OPTOFORK);
             motor_steps++;
         }
-        if (edge > 0) {
-            printf("%d. revolution: %d steps\n", edge, motor_steps);
-        }
-        if (edge > 0) {
-            results[edge - 1] = motor_steps;
+        if (i == 0) { // prints just fyi first (possibly incomplete) round steps and then the full revolution steps
+            printf("Step count to first falling edge: %d\n", motor_steps);
+        } else {
+            printf("Step count of a full revolution: %d\n", motor_steps);
         }
     }
-    int average = (results[0] + results[1] + results[2]) / 3;
-    printf("Average step count per revolution = %d\n", average);
+    for (int i = 0; i < 130; i++) { // offsetting the about 130 step disparency between the hole and the motor
+        step_motor();
+        sleep_ms(1);
+    }
     return motor_steps;
 }
 
@@ -59,24 +76,23 @@ void set_up_gpio(void) {
     gpio_set_dir(STEPM4, GPIO_OUT);
 }
 
-void step_motor(int index) {
-    gpio_put(STEPM1, steps[index][0]);
-    gpio_put(STEPM2, steps[index][1]);
-    gpio_put(STEPM3, steps[index][2]);
-    gpio_put(STEPM4, steps[index][3]);
+void step_motor(void) {
+    static int step_index = 0;
+    step_index = (step_index + 1) % 8;
+    gpio_put(STEPM1, steps[step_index][0]);
+    gpio_put(STEPM2, steps[step_index][1]);
+    gpio_put(STEPM3, steps[step_index][2]);
+    gpio_put(STEPM4, steps[step_index][3]);
 }
 
 int run_motor(int run_times, int motor_steps) {
     int take_steps_amount = run_times * motor_steps / 8;
     int counter = 0;
 
-    int step_index = 0;
     for (int i = 0; i < take_steps_amount; i++) {
-        step_motor(step_index);
-        step_index = (step_index + 1) % 8;
+        step_motor();
         counter++;
-        sleep_ms(2);
+        sleep_ms(1);
     }
     return counter;
 }
-
